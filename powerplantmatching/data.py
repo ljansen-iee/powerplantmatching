06fba69ab01f99ce90cdd540fcd307acd7b8c232
Lukas Jansen <lukas.jansen@uni-kassel.de>
2023-11-13 17:54:41 +0100

    WEPP pipe reordering

--- powerplantmatching/data.py
+++ powerplantmatching/data.py
@@ -1118,9 +1118,9 @@ def WEPP(raw=False, config=None):
     return (
         wepp.pipe(set_column_name, "WEPP")
         .pipe(config_filter, config)
+        .pipe(fill_missing_decommissioning_years, config)
         .pipe(fill_geoposition, saved_only=False) # use saved_only=False + google_api_key initially
         .pipe(scale_to_net_capacities, (not config["WEPP"]["net_capacity"]))
-        .pipe(fill_missing_decommissioning_years, config)
         .pipe(correct_manually, "WEPP", config=config)
     )
 
